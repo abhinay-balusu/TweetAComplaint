10ae1862c2262a12ba2e1921a524c5c4467260b7
Abhinay <balusu.abhinay@gmail.com>
2016-09-13 17:52:22 -0400

    removed db credentials

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-$connection = mysql_connect('127.12.123.2:3306', 'adminS1S5VEn', 'Z6X7lnKccakM');
+$connection = mysql_connect('host', 'username', 'password');
 mysql_select_db('tweet_a_complaint');
 
 if (!$connection) {
